8f513bd0d1aa33fc9e0713d57c2ee89d6059a713
axquijano <axquiijano@unicauca.edu.co>
2023-06-15 23:59:30 -0500

    Cambio de tema y actualizacion de inicio

--- app/src/main/java/co/edu/appmovilsmartcoach/screens/home/Home.kt
+++ app/src/main/java/co/edu/appmovilsmartcoach/screens/home/Home.kt
@@ -17,6 +17,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.currentBackStackEntryAsState
@@ -164,7 +165,7 @@ fun TopBar(
     }
 
     TopAppBar (
-        title= { Text(text = MyTitle)},
+        title= { Text(text = MyTitle, fontSize = 15.sp)},
         //Boton para abrir el boton lateral
         navigationIcon = {
             IconButton(onClick = {
--- app/src/main/java/co/edu/appmovilsmartcoach/ui/theme/Color.kt
+++ app/src/main/java/co/edu/appmovilsmartcoach/ui/theme/Color.kt
@@ -3,6 +3,6 @@ package co.edu.appmovilsmartcoach.ui.theme
 import androidx.compose.ui.graphics.Color
 
 val Purple200 = Color(0xFF475F9C)
-val Purple500 = Color(0xFF3C4432)
+val Purple500 = Color(0xFF000000)
 val Purple700 = Color(0xFF5F579B)
 val Teal200 = Color(0xFFFFFFFF)
